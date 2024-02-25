2646435535ba113878c4b542015a8b738afd2b98
shahabcodes <shahabusaid.mm@gmail.com>
2024-02-25 13:16:45 +0300

    header component completed

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,5 +9,14 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  //For capturing the image from browse component
+  @Input({required: true}) userImg: string = '';
+  @Input({required: true}) userName: string = '';
+  firstName: string = '';
   navList = ["Home", "TV Shows", "News & Popular", "My List", "Browse by Language"]
+
+  ngOnInit() {
+    this.firstName = this.userName.split(' ')[0];
+    // Now firstName contains only the first name
+}
 }
